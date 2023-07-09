700443f3cc9df0f2efdedceb972260312e74fb7b
zhu410289616 <zhu410289616@163.com>
2023-07-09 13:58:12 +0800

    调整 压缩 文件 形式

--- unzip_files_by_tar.sh
+++ unzip_files_by_tar.sh
@@ -4,11 +4,15 @@
 FDK_AAC_NAME="fdk-acc-ios-2.0.2.tar.bz2"
 
 tar -jxvf $FDK_AAC_NAME
+cp -r fdk-aac-ios/include/ FFmpeg-iOS/include
+cp -r fdk-aac-ios/lib/ FFmpeg-iOS/lib
 
 # x264 file name
 X264_NAME="x264-iOS-20191217-2245.tar.bz2"
 
 tar -jxvf $X264_NAME
+cp -r x264-iOS/include/ FFmpeg-iOS/include
+cp -r x264-iOS/lib/ FFmpeg-iOS/lib
 
 # ffmpeg file name
 FFMPEG_NAME="ffmpeg-4.3.1-zip"
@@ -23,3 +27,6 @@ for LIB in $FFMPEG_NAME_LIB/*.tar.bz2
 do
 	tar -jxvf $LIB
 done
+
+cp -r $FFMPEG_NAME/include/ FFmpeg-iOS/include
+cp -r $FFMPEG_NAME/lib/ FFmpeg-iOS/lib
