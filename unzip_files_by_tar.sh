0aa7065e7d1d014b35f2c04d526c004dd8c2ae23
zhu410289616 <zhu410289616@163.com>
2023-07-09 12:17:43 +0800

    增加 tar 脚本

---
+++ unzip_files_by_tar.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# fdk-acc file name
+FDK_AAC_NAME="fdk-acc-ios-2.0.2.tar.bz2"
+
+tar -jxvf $FDK_AAC_NAME
+
+# x264 file name
+X264_NAME="x264-iOS-20191217-2245.tar.bz2"
+
+tar -jxvf $X264_NAME
+
+# ffmpeg file name
+FFMPEG_NAME="ffmpeg-4.3.1-zip"
+FFMPEG_NAME_INCLUDE="$FFMPEG_NAME/include"
+FFMPEG_NAME_LIB="$FFMPEG_NAME/lib"
+
+# include dir
+tar -jxvf $FFMPEG_NAME_INCLUDE.tar.bz2
+
+# lib files
+for LIB in $FFMPEG_NAME_LIB/*.tar.bz2
+do
+	tar -jxvf $LIB
+done
